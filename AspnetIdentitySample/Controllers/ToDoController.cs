2c8994af5982e7ab813499c9132054380cccd57f
rustd <pranavra@PRANAVRA0808.redmond.corp.microsoft.com>
2013-10-18 19:52:51 -0700

    Add ToDo functionality and associate Users with ToDo

---
+++ AspnetIdentitySample/Controllers/ToDoController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AspnetIdentitySample.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace AspnetIdentitySample.Controllers
+{
+    [Authorize]
+    public class ToDoController : Controller
+    {
+        private MyDbContext db;
+        private UserManager<MyUser> manager;
+        public ToDoController()
+        {
+            db = new MyDbContext();
+            manager = new UserManager<MyUser>(new UserStore<MyUser>(db));
+        }
+        
+        // GET: /ToDo/
+        // GET ToDo for the logged in user
+        public ActionResult Index()
+        {
+            var currentUser = manager.FindById(User.Identity.GetUserId());
+            return View(db.ToDoes.ToList().Where(todo => todo.User.Id == currentUser.Id));
+        }
+
+        // GET: /ToDo/All
+        [Authorize(Roles="Admin")]
+        public async Task<ActionResult> All()
+        {
+            return View(await db.ToDoes.ToListAsync());
+        }
+
+        // GET: /ToDo/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            var currentUser = await manager.FindByIdAsync(User.Identity.GetUserId()); 
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ToDo todo = await db.ToDoes.FindAsync(id);
+            if (todo == null)
+            {
+                return HttpNotFound();
+            }
+            if (todo.User.Id != currentUser.Id)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.Unauthorized);
+            }
+            return View(todo);
+        }
+
+        // GET: /ToDo/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /ToDo/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include="Id,Description,IsDone")] ToDo todo)
+        {
+            var currentUser = await manager.FindByIdAsync(User.Identity.GetUserId()); 
+            if (ModelState.IsValid)
+            {
+                todo.User = currentUser;
+                db.ToDoes.Add(todo);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(todo);
+        }
+
+        // GET: /ToDo/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            var currentUser = await manager.FindByIdAsync(User.Identity.GetUserId()); 
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ToDo todo = await db.ToDoes.FindAsync(id);
+            if (todo == null)
+            {
+                return HttpNotFound();
+            }
+            if (todo.User.Id != currentUser.Id)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.Unauthorized);
+            }
+            return View(todo);
+        }
+
+        // POST: /ToDo/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include="Id,Description,IsDone")] ToDo todo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(todo).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(todo);
+        }
+
+        // GET: /ToDo/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            var currentUser = await manager.FindByIdAsync(User.Identity.GetUserId()); 
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ToDo todo = await db.ToDoes.FindAsync(id);
+            if (todo == null)
+            {
+                return HttpNotFound();
+            }
+            if (todo.User.Id != currentUser.Id)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.Unauthorized);
+            } 
+            return View(todo);
+        }
+
+        // POST: /ToDo/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            ToDo todo = await db.ToDoes.FindAsync(id);
+            db.ToDoes.Remove(todo);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
